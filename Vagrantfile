9335d2bfc066e5d459c87d086b738dc9eb7e307c
Erik Torsner <erik@torgesta.com>
2016-02-07 16:40:10 +0100

    adding

---
+++ Vagrantfile
@@ -0,0 +1,11 @@
+# -*- mode: ruby -*-
+# vi: set ft=ruby :
+
+HOSTNAME = 'local'
+IPNUMBER = '192.168.50.999'
+DEVDNS   = 'www.wordpress.local'
+TESTDNS  = 'test.wordpress.local'
+STAGEDNS  = 'staging.wordpress.local'
+
+dir = Dir.pwd
+require dir + '/vagrant/Vagrant'
---
+++ vagrant/database/init.sql
@@ -0,0 +1,12 @@
+# We include default installations of WordPress with this Vagrant setup.
+# In order for that to respond properly, default databases should be
+# available for use.
+CREATE DATABASE IF NOT EXISTS `wordpress`;
+GRANT ALL PRIVILEGES ON `wordpress`.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';
+
+CREATE DATABASE IF NOT EXISTS `wordpress-test`;
+GRANT ALL PRIVILEGES ON `wordpress-test`.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';
+
+CREATE DATABASE IF NOT EXISTS `wordpress-staging`;
+GRANT ALL PRIVILEGES ON `wordpress-staging`.* TO 'wordpress'@'localhost' IDENTIFIED BY 'wordpress';
+
